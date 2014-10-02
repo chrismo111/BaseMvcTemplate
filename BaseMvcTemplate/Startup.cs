61542e6f852c6d02ccbe3e839f47db147e48fa4e
Chris <chris.mohan.cf@gmail.com>
2014-10-02 15:16:36 -0400

    Initial Commit II

---
+++ BaseMvcTemplate/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BaseMvcTemplate.Startup))]
+namespace BaseMvcTemplate
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
